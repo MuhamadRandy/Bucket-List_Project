7201c95be316510f4a0d0f6064ae6599709e9f89
Muhamad Randy <m.randyaranda@gmail.com>
2023-10-10 22:51:56 +0700

    Bucket List

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.z2gxd4g.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -12,36 +21,39 @@ def home():
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    bucket_receive = request.form["bucket_give"]
+    # sample_receive = request.form['sample_give']
+    bucket_receive = request.form['bucket_give']
+
     count = db.bucket.count_documents({})
     num = count + 1
+
     doc = {
-        'num':num,
+        'num': num,
         'bucket': bucket_receive,
-        'done':0
+        'done': 0,
     }
     db.bucket.insert_one(doc)
-    return jsonify({'msg':'data saved!'})
+    return jsonify({'msg': 'data saved!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
-    num_receive = request.form["num_give"]
+    num_receive = request.form['num_give']
     db.bucket.update_one(
         {'num': int(num_receive)},
         {'$set': {'done': 1}}
     )
-    return jsonify({'msg': 'Update done!'})
+    return jsonify({'msg': 'update done!'})
+
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': 'delete done!'})
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    buckets_list = list(db.bucket.find({},{'_id':False}))
-    return jsonify({'buckets':buckets_list})\
-
-@app.route('/bucket/delete', methods=['POST'])
-def bucket_delete():
-    num_receive = request.form["num_give"]
-    db.bucket.delete_one({'num' : int(num_receive)})
-    return jsonify ({'msg' : 'Delete done!'})
+    buckets_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'buckets': buckets_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
